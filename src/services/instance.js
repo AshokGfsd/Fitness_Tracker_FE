be4fb9c5f0a29626de1f0fb68a1faa2894e32679
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-07-28 10:05:33 +0530

    first commit

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:4444";
+const baseURL = "https://fitness-tracker-be-s2gj.onrender.com/";
 // const baseURL = "https://fitness-tracker-server-86vy.onrender.com/fitness/";
 
 const instance = axios.create({
